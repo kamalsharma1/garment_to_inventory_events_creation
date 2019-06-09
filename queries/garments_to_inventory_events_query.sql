3f40bd016303e18dbc44c9157a24b021c7e0fc0e
kamalsharma1 <kamal.sharma@mediaagility.com>
2019-06-09 18:00:26 -0400

    update

--- queries/garments_to_inventory_events_query.sql
+++ queries/garments_to_inventory_events_query.sql
@@ -1,12 +1,20 @@
 INSERT INTO `INVENTORY_EVENTS_TABLE`
 
 With create_events as (
-  SELECT *
+  SELECT skuId, receivedDate, poId, cost, id, fingerprint, 
+      CASE 
+        WHEN locationId IS NULL THEN "NOT_FOUND"
+        ELSE  locationId
+      END as locationId,
+      CASE 
+        WHEN locationDesc IS NULL THEN "NOT_FOUND"
+        ELSE locationDesc
+      END as locationDesc ,
+      event_type, event_subtype, event_datetime
   FROM
     `GARMENT_EVENTS_TABLE`
   WHERE
     event_subtype IN ("garment_event","garment_create")
-    AND locationId IS NOT NULL and locationDesc IS NOT NULL
 )
 
 (
@@ -14,8 +22,8 @@ With create_events as (
     create_event.poId AS po_id,
     delete_event.id AS garment_id,
     create_event.skuId AS sku_id,
-    delete_event.event_type,
-    delete_event.event_subtype,
+    "GARMENT_DELETE" as event_type,
+    "GARMENT_DELETE" as event_subtype,
     delete_event.event_datetime,
     -create_event.cost AS unit_cost,
     create_event.locationid AS location_id,
@@ -24,12 +32,20 @@ With create_events as (
     -1 AS quantity_received
   FROM
     (
-      SELECT *
+      SELECT skuId, receivedDate, poId, cost, id, fingerprint, 
+      CASE 
+        WHEN locationId IS NULL THEN "NOT_FOUND"
+        ELSE  locationId
+      END as locationId,
+      CASE 
+        WHEN locationDesc IS NULL THEN "NOT_FOUND"
+        ELSE locationDesc
+      END as locationDesc ,
+      event_type, event_subtype, event_datetime
       FROM
         `GARMENT_EVENTS_TABLE`
       WHERE
         event_subtype =("garment_delete") 
-        AND locationId IS NOT NULL and locationDesc IS NOT NULL
     ) as delete_event
   LEFT JOIN 
     create_events as create_event
@@ -41,8 +57,8 @@ UNION ALL
     create_event.poId AS po_id,
     create_event.id AS garment_id,
     create_event.skuId AS sku_id,
-    create_event.event_type,
-    create_event.event_subtype,
+    "INITIAL_RECEIPT" as event_type,
+    "GARMENT_CREATE" as event_subtype,
     create_event.event_datetime,
     create_event.cost AS unit_cost,
     create_event.locationid AS location_id,
